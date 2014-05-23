e29710d705196d473095a577e19be6d895e843e6
Allen Hurlbert <hurlbert@bio.unc.edu>
2014-05-23 16:13:08 -0400

    added new modified functions so that quantiles() will not break when passed rates of NAs

--- bamm_plotting.r
+++ bamm_plotting.r
@@ -11,6 +11,184 @@ require(ape)
 require(BAMMtools)
 require(RColorBrewer)
 
+# Temporary functions replacing assignColorBreaks and plot.bammdata
+# to remedy a problem with passing rates of NA to quantiles() within
+# assignColorBreaks. Here, I've simply added a na.rm = TRUE argument.
+assignColorBreaks2 = function (rates, NCOLORS = 64, spex = "s") 
+{
+  if (mode(rates) == "numeric") {
+    bks <- quantile(rates, seq(0, 1, length.out = (NCOLORS + 
+                                                     1)), na.rm = TRUE)
+  }
+  else if (mode(rates) == "list") {
+    if (tolower(spex) == "s") {
+      bks <- quantile(rates[[1]], seq(0, 1, length.out = (NCOLORS + 
+                                                            1)), na.rm = TRUE)
+    }
+    else if (tolower(spex) == "e") {
+      bks <- quantile(rates[[2]], seq(0, 1, length.out = (NCOLORS + 
+                                                            1)), na.rm = TRUE)
+    }
+    else {
+      bks <- quantile(rates[[1]] - rates[[2]], seq(0, 1, 
+                                                   length.out = (NCOLORS + 1)), na.rm = TRUE)
+    }
+  }
+  return(bks)
+}
+
+# And now plot.bammdata2 simply calls assignColorBreaks2 instead of assignColorBreaks
+plot.bammdata2 = function (x, method = "phylogram", vtheta = 5, rbf = 0.001, show = TRUE, 
+                           labels = FALSE, legend = FALSE, spex = "s", lwd = 1, cex = 1, 
+                           pal = "RdYlBu", colorbreaks = NULL, par.reset = TRUE, ...) 
+{
+  if ("bammdata" %in% class(x)) {
+    if (attributes(x)$order != "cladewise") {
+      stop("Function requires tree in 'cladewise' order")
+    }
+    phy <- as.phylo.bammdata(x)
+  }
+  else stop("Object ephy must be of class bammdata")
+  if (!is.binary.tree(phy)) {
+    stop("Function requires fully bifurcating tree")
+  }
+  if (any(phy$edge.length == 0)) {
+    warning("Tree contains zero length branches. Rates for these will be NA and coerced to zero")
+  }
+  if (!("dtrates" %in% names(x))) {
+    x <- dtRates(x, 0.01)
+  }
+  if (is.null(colorbreaks)) {
+    colorbreaks <- assignColorBreaks2(x$dtrates$rates, 64, 
+                                      spex)
+  }
+  if (x$type == "trait") {
+    if (sum(is.na(x$dtrates$rates))) {
+      warning(sprintf("Found %d NA phenotypic rates. Coercing to zero.", 
+                      sum(is.na(x$dtrates$rates))))
+      x$dtrates$rates[is.na(x$dtrates$rates)] <- 0
+    }
+    colorobj <- colorMap(x$dtrates$rates, pal, colorbreaks)
+  }
+  else if (x$type == "diversification") {
+    if (sum(is.na(x$dtrates$rates[[1]]))) {
+      warning(sprintf("Found %d NA speciation rates. Coercing to zero.", 
+                      sum(is.na(x$dtrates$rates[[1]]))))
+      x$dtrates$rates[[1]][is.na(x$dtrates$rates[[1]])] <- 0
+    }
+    if (sum(is.na(x$dtrates$rates[[2]]))) {
+      warning(sprintf("Found %d NA extinction rates. Coercing to zero.", 
+                      sum(is.na(x$dtrates$rates[[2]]))))
+      x$dtrates$rates[[2]][is.na(x$dtrates$rates[[2]])] <- 0
+    }
+    if (tolower(spex) == "s") {
+      colorobj <- colorMap(x$dtrates$rates[[1]], pal, colorbreaks)
+    }
+    else if (tolower(spex) == "e") {
+      colorobj <- colorMap(x$dtrates$rates[[2]], pal, colorbreaks)
+    }
+    else {
+      colorobj <- colorMap(x$dtrates$rates[[1]] - x$dtrates$rates[[2]], 
+                           pal, colorbreaks)
+    }
+  }
+  else {
+    stop("Unrecognized/corrupt bammdata class. Type does not equal 'trait' or 'diversification'")
+  }
+  edge.color <- colorobj$cols
+  tH <- max(branching.times(phy))
+  phy$begin <- x$begin
+  phy$end <- x$end
+  tau <- x$dtrates$tau
+  if (method == "polar") {
+    ret <- setPolarTreeCoords(phy, vtheta, rbf)
+    rb <- tH * rbf
+    p <- mkdtsegsPolar(ret$segs[-1, ], tau, x$edge)
+  }
+  else if (method == "phylogram") {
+    ret <- setPhyloTreeCoords(phy)
+    p <- mkdtsegsPhylo(ret$segs[-1, ], tau, x$edge)
+  }
+  else {
+    stop("Unimplemented method")
+  }
+  x0 <- c(ret$segs[1, 1], p[, 1])
+  x1 <- c(ret$segs[1, 3], p[, 2])
+  y0 <- c(ret$segs[1, 2], p[, 3])
+  y1 <- c(ret$segs[1, 4], p[, 4])
+  offset <- table(p[, 5])[as.character(unique(p[, 5]))]
+  arc.color <- c(edge.color[1], edge.color[match(unique(p[, 
+                                                          5]), p[, 5]) + offset])
+  edge.color <- c(edge.color[1], edge.color)
+  if (show) {
+    if (length(list(...))) {
+      op <- par(no.readonly = TRUE)
+      par(...)
+    }
+    plot.new()
+    ofs <- 0
+    if (labels) {
+      ofs <- max(nchar(phy$tip.label) * 0.03 * cex)
+    }
+    if (method == "polar") {
+      plot.window(xlim = c(-1, 1) + c(-rb, rb) + c(-ofs, 
+                                                   ofs), ylim = c(-1, 1) + c(-rb, rb) + c(-ofs, 
+                                                                                          ofs), asp = 1)
+      segments(x0, y0, x1, y1, col = edge.color, lwd = lwd, 
+               lend = 2)
+      arc(0, 0, ret$arcs[, 1], ret$arcs[, 2], c(rb, rb + 
+                                                  phy$end/tH), border = arc.color, lwd = lwd)
+      if (labels) {
+        for (k in 1:length(phy$tip.label)) {
+          text(ret$segs[-1, ][phy$edge[, 2] == k, 3], 
+               ret$segs[-1, ][phy$edge[, 2] == k, 4], phy$tip.label[k], 
+               cex = cex, srt = (180/pi) * ret$arcs[-1, 
+                                                    ][phy$edge[, 2] == k, 1], adj = c(0, NA))
+        }
+      }
+    }
+    if (method == "phylogram") {
+      plot.window(xlim = c(0, 1 + ofs), ylim = c(0, phy$Nnode * 
+                                                   1/(phy$Nnode + 1)))
+      segments(x0[-1], y0[-1], x1[-1], y1[-1], col = edge.color[-1], 
+               lwd = lwd, lend = 2)
+      isTip <- phy$edge[, 2] <= phy$Nnode + 1
+      isTip <- c(FALSE, isTip)
+      segments(ret$arcs[!isTip, 1], ret$arcs[!isTip, 2], 
+               ret$arcs[!isTip, 3], ret$arcs[!isTip, 4], col = arc.color[!isTip], 
+               lwd = lwd, lend = 2)
+      if (labels) {
+        text(ret$segs[-1, ][phy$edge[, 2] <= phy$Nnode + 
+                              1, 3], ret$segs[-1, ][phy$edge[, 2] <= phy$Nnode + 
+                                                      1, 4], phy$tip.label, cex = cex, pos = 4, offset = 0.25)
+      }
+    }
+    if (legend) {
+      rateLegend(colorobj$colsdensity)
+    }
+  }
+  index <- order(as.numeric(rownames(ret$segs)))
+  if (method == "phylogram") {
+    assign("last_plot.phylo", list(type = "phylogram", direction = "rightwards", 
+                                   Ntip = phy$Nnode + 1, Nnode = phy$Nnode, edge = phy$edge, 
+                                   xx = ret$segs[index, 3], yy = ret$segs[index, 4], 
+                                   pp = par(no.readonly = TRUE)), envir = .PlotPhyloEnv)
+  }
+  else if (method == "polar") {
+    assign("last_plot.phylo", list(type = "fan", Ntip = phy$Nnode + 
+                                     1, Nnode = phy$Nnode, edge = phy$edge, xx = ret$segs[index, 
+                                                                                          3], yy = ret$segs[index, 4], theta = ret$segs[index, 
+                                                                                                                                        5], rb = rb, pp = par(no.readonly = TRUE)), envir = .PlotPhyloEnv)
+  }
+  if (par.reset) {
+    if (length(list(...))) {
+      par(op)
+    }
+  }
+  invisible(list(coords = ret$segs[-1, ], colorbreaks = colorbreaks, 
+                 colordens = colorobj$colsdensity))
+}
+
 
 bamm.plot = function(tree, edata, extantPops, colorbreaks = NULL, offset = 0.03) {
   spp = unique(extantPops$spp.name)
@@ -27,7 +205,8 @@ bamm.plot = function(tree, edata, extantPops, colorbreaks = NULL, offset = 0.03)
   tips = data.frame(spp.name = as.character(tree$tip.label))
   home.regions = merge(tips, home.reg, by = 'spp.name', sort = F)
   
-  xx = plot.bammdata(edata, lwd=1, colorbreaks = colorbreaks)  
+  #Switch back to plot.bammdata if the quantile(na.rm = TRUE) gets fixed in source
+  xx = plot.bammdata2(edata, lwd=1, colorbreaks = colorbreaks)  
   cols = colorRampPalette(c('violet', 'white','darkgreen'))(10)
   tiplabels(pch = 15, adj = (0.5 + offset), cex = 2, col = cols[home.regions$region])
   return(xx)
